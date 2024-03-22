24f82bf90bc0a7d8619d14b1f523dd5e959d1a7b
Gaurav Singh <145368335+GauravSingh003@users.noreply.github.com>
2024-03-22 14:24:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $('main').mouseenter(function(){
+    $('main').css("background-color",'gray')
+});
+$('main').mouseleave(function(){
+    $('main').css("background-color",'white')
+});
+});
